7072055bee1add4526c8becbc0c35bfe41bade87
Amit Vashist <amitvashist7@gmail.com>
2018-08-08 08:04:26 +0000

    Copy Static Local file & remote file

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'amitvashist7@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.2.0'
+version '0.2.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 depends 'workstation'
--- cookbooks/apache/recipes/webpage.rb
+++ cookbooks/apache/recipes/webpage.rb
@@ -1,3 +1,9 @@
+remote_file '/var/www/html/mario.jpg' do
+source 'https://banner2.kisspng.com/20171218/cb3/mario-png-5a37d12acbdad3.967879731513607466835.jpg'
+end
+
+
+
 template '/var/www/html/index.html' do 
 source 'index.erb'
 action :create
@@ -6,4 +12,10 @@ variables(
  :lastname => 'Cloud'
 )
 end
+
+
+cookbook_file '/var/www/html/abc.html' do
+ source 'abc.html'
+end
+
 
